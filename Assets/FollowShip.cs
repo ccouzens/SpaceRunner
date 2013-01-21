f87db06a61656512f0117d95e5646de268b7807b
Chris Couzens <ccouzens@gmail.com>
2013-01-21 21:16:45 +0000

    Don't move the screen left and right with the ship

--- Assets/FollowShip.cs
+++ Assets/FollowShip.cs
@@ -4,14 +4,19 @@ using System.Collections;
 public class FollowShip : MonoBehaviour {
 	public Transform ship;
 	private Vector3 offset;
+	private float horizontalPosition;
 
 	// Use this for initialization
 	void Start () {
 		offset = transform.position - ship.position;
+		horizontalPosition = ship.position.x;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = ship.position + offset;
+		Vector3 p = transform.position;
+		p = ship.position + offset;
+		p.x = horizontalPosition;
+		transform.position = p;
 	}
 }
